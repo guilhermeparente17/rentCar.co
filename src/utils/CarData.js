7d488f2b801d5d042861aaec8ca9295cacccc3c2
Guilherme Parente <guilherme.parente17@outlook.com>
2024-01-14 13:52:09 -0300

    Adicionando dados fake para catalogo

--- src/utils/CarData.js
+++ src/utils/CarData.js
@@ -19,26 +19,26 @@ export const CarData = [
     {
         id: 2,
         category: 'SUV',
-        imageUrl: 'https://clienteparasempre.com.br/Chevrolet/consorcio/detalhes/99.png',
-        brand: 'Chevrolet',
-        name: 'Tracker AT',
-        stars: 4,
+        imageUrl: 'https://ofertas.vw.com.br/wp-content/uploads/2022/08/Nivus_Confortline_200TSI_Preto_MY23_3_4_Frente_551x301px-1.png',
+        brand: 'Volkswagen ',
+        name: 'Nivus Comfortline',
+        stars: 5,
         additional: {
             cambio: 'Automatic',
             arCondicionado: true,
-            peoples: 6,
+            peoples: 5,
         },
         value: {
             currency: '$',
-            amount: 300,
+            amount: 400,
         }
     },
     {
         id: 3,
         category: 'SUV',
-        imageUrl: 'https://clienteparasempre.com.br/Chevrolet/consorcio/detalhes/99.png',
-        brand: 'Chevrolet',
-        name: 'Tracker AT',
+        imageUrl: 'https://www.hyundaiamx.com.br/assets/uploads/nt_veiculos_cores/17620-Cores-Azul-Sapphire---550x325.png',
+        brand: 'Hyundai',
+        name: 'Creta Action',
         stars: 4,
         additional: {
             cambio: 'Automatic',
@@ -53,10 +53,10 @@ export const CarData = [
     {
         id: 5,
         category: 'SPORT',
-        imageUrl: 'https://clienteparasempre.com.br/Chevrolet/consorcio/detalhes/99.png',
-        brand: 'Chevrolet',
-        name: 'Tracker AT',
-        stars: 2,
+        imageUrl: 'https://cdn.drivek.com/configurator-imgs/cars/it/$original$/FERRARI/PUROSANGUE/41365_SUV-5-DOORS/ferrari-purosangue.png',
+        brand: 'Ferrari',
+        name: 'Purosangue',
+        stars: 4,
         additional: {
             cambio: 'Automatic',
             arCondicionado: true,
@@ -64,20 +64,20 @@ export const CarData = [
         },
         value: {
             currency: '$',
-            amount: 300,
+            amount: 700,
         }
     },
     {
         id: 6,
         category: 'SPORT',
-        imageUrl: 'https://clienteparasempre.com.br/Chevrolet/consorcio/detalhes/99.png',
-        brand: 'Chevrolet',
-        name: 'Tracker AT',
-        stars: 3,
+        imageUrl: 'https://vexstockimages.fastly.carvana.io/stockimages/2021_HONDA_CIVIC%20TYPE%20R_TOURING%20HATCHBACK%20SEDAN%204D_WHITE_stock_desktop_1920x1080.png?v=1645596567.409',
+        brand: 'Honda',
+        name: 'Civic Type R',
+        stars: 4,
         additional: {
             cambio: 'Automatic',
             arCondicionado: true,
-            peoples: 6,
+            peoples: 4,
         },
         value: {
             currency: '$',
@@ -87,9 +87,9 @@ export const CarData = [
     {
         id: 9,
         category: 'WAGON',
-        imageUrl: 'https://clienteparasempre.com.br/Chevrolet/consorcio/detalhes/99.png',
-        brand: 'Chevrolet',
-        name: 'Tracker AT',
+        imageUrl: 'https://cdn.nwi-ms.com/media/pt/A/mc/8WDCJGN0/model/19front.png?F=&P=&M=&flag=transparent',
+        brand: 'Audi',
+        name: 'A4 Avant',
         stars: 5,
         additional: {
             cambio: 'Automatic',
@@ -98,16 +98,16 @@ export const CarData = [
         },
         value: {
             currency: '$',
-            amount: 300,
+            amount: 1000,
         }
     },
     {
         id: 10,
         category: 'WAGON',
-        imageUrl: 'https://clienteparasempre.com.br/Chevrolet/consorcio/detalhes/99.png',
-        brand: 'Chevrolet',
-        name: 'Tracker AT',
-        stars: 4,
+        imageUrl: 'https://cdn.dealerk.it/dealer/datafiles/vehicle/images/480x0/2065/vsuIjMckpdzi7UUH.png',
+        brand: 'Audi',
+        name: 'Audi RS4',
+        stars: 5,
         additional: {
             cambio: 'Automatic',
             arCondicionado: true,
@@ -115,16 +115,16 @@ export const CarData = [
         },
         value: {
             currency: '$',
-            amount: 300,
+            amount: 900,
         }
     },
     {
         id: 11,
         category: 'OFFROAD',
-        imageUrl: 'https://clienteparasempre.com.br/Chevrolet/consorcio/detalhes/99.png',
-        brand: 'Chevrolet',
-        name: 'Tracker AT',
-        stars: 4,
+        imageUrl: 'https://pngimg.com/uploads/land_rover/land_rover_PNG64.png',
+        brand: 'Land Rover',
+        name: 'Land Rover Defender',
+        stars: 5,
         additional: {
             cambio: 'Automatic',
             arCondicionado: true,
@@ -132,16 +132,16 @@ export const CarData = [
         },
         value: {
             currency: '$',
-            amount: 300,
+            amount: 800,
         }
     },
     {
         id: 12,
         category: 'OFFROAD',
-        imageUrl: 'https://clienteparasempre.com.br/Chevrolet/consorcio/detalhes/99.png',
-        brand: 'Chevrolet',
-        name: 'Tracker AT',
-        stars: 4,
+        imageUrl: 'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_default/v1/editorial/vhs/2023-Hilux-GRS-SAL-alt.png',
+        brand: 'Toyota',
+        name: 'Toyota Hilux',
+        stars: 5,
         additional: {
             cambio: 'Automatic',
             arCondicionado: true,
@@ -149,7 +149,7 @@ export const CarData = [
         },
         value: {
             currency: '$',
-            amount: 300,
+            amount: 700,
         }
     },
 ];
